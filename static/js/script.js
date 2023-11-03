1545c8719258b147351b7864543685684f94ec0d
Nick Smith <nicksmith100@yahoo.com>
2023-11-03 16:49:13 +0000

    Add confirmation modal for delete admin function

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+// Display confirmation modal with dynamic confirm button
+
+let confirmationModal = document.getElementById('modal-confirm');
+
+confirmationModal.addEventListener('show.bs.modal', function (event) {
+
+// Button that triggered the modal
+let button = event.relatedTarget;
+
+// Extract info from data-bs-url attribute
+let url = button.getAttribute('data-bs-url');
+
+// Update the modal's text content
+let modalText = confirmationModal.querySelector('.modal-text');
+modalText.innerHTML = `<strong class="text-danger"><i class="bi bi-exclamation-circle-fill" aria-hidden="true"> </i>Are you sure? This action cannot be undone!</strong>`;
+
+// Update the modal's button content
+let modalConfirm = confirmationModal.querySelector('.btn-confirm');
+modalConfirm.href = url;
+
+});
