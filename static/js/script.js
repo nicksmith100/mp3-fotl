03dea5b6d96ff2f0cf5517426f375538346e8fa7
Nick Smith <nicksmith100@yahoo.com>
2023-11-14 22:02:52 +0000

    Add jquery toggle for biography on lineup page

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,50 @@
-/* Display confirmation modal with dynamic confirm button */
+$(document).ready(function(){
 
-let confirmationModal = document.getElementById('modal-confirm');
-confirmationModal.addEventListener('show.bs.modal', function (event) {
+    /* Display confirmation modal with dynamic confirm button */
 
-// Button that triggered the modal
-let button = event.relatedTarget;
+    let confirmationModal = document.getElementById('modal-confirm');
+    confirmationModal.addEventListener('show.bs.modal', function (event) {
 
-// Extract info from data-bs-url attribute
-let url = button.getAttribute('data-bs-url');
+        // Button that triggered the modal
+        let button = event.relatedTarget;
 
-// Update the modal's text content
-let modalText = confirmationModal.querySelector('.modal-text');
-modalText.innerHTML = `<strong class="text-danger"><i class="bi bi-exclamation-circle-fill" aria-hidden="true"> </i>Are you sure? This action cannot be undone!</strong>`;
+        // Extract info from data-bs-url attribute
+        let url = button.getAttribute('data-bs-url');
 
-// Update the modal's button content
-let modalConfirm = confirmationModal.querySelector('.btn-confirm');
-modalConfirm.href = url;
+        // Update the modal's text content
+        let modalText = confirmationModal.querySelector('.modal-text');
+        modalText.innerHTML = `<strong class="text-danger"><i class="bi bi-exclamation-circle-fill" aria-hidden="true"> </i>Are you sure? This action cannot be undone!</strong>`;
 
-});
+        // Update the modal's button content
+        let modalConfirm = confirmationModal.querySelector('.btn-confirm');
+        modalConfirm.href = url;
 
-/* Date-and-time picker using flatpickr plugin */
+    });
 
-$(".showtime").flatpickr({
-    enableTime: true,
-    dateFormat: "d-m-Y H:i",
-    minDate: "13-07-2023",
-    maxDate: "16-07-2023"
-});
+    /* Date-and-time picker using flatpickr plugin */
 
-/* Date picker using flatpickr plugin */
+    $(".showtime").flatpickr({
+        enableTime: true,
+        dateFormat: "d-m-Y H:i",
+        minDate: "13-07-2023",
+        maxDate: "16-07-2023"
+    });
 
-$(".event-date").flatpickr({
-    dateFormat: "d-m-Y",
-    minDate: "01-07-2022",
+    /* Date picker using flatpickr plugin */
+
+    $(".event-date").flatpickr({
+        dateFormat: "d-m-Y",
+        minDate: "01-07-2022",
+    });
+
+    function handler( event ) {
+    var target = $( event.target );
+    if ( target.is( "button" ) ) {
+        target.children().toggleClass("bi-caret-down-fill bi-caret-up-fill");
+        target.next().toggle(1000);
+    }
+    }
+    
+    $( "button" ).on( "click", handler );
+    
 });
